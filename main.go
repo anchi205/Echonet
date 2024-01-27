2afaeff898b6eae1eb41543820418c8fac9d7f8e
anchi205 <borahanchita@gmail.com>
2024-01-27 19:31:31 +0530

    Setup logging configs

--- main.go
+++ main.go
@@ -1,6 +1,26 @@
 package main
 
-import "github.com/anchi205/Echonet/src"
+import (
+	"os" // way to interact with the OS, such as reading envt variables.
+
+	"github.com/anchi205/Echonet/src"
+	"github.com/sirupsen/logrus" // structured logging.
+)
+
+func init() { // called automatically before the main function.
+	// It sets the log format to a colored, timestamped text format
+	logrus.SetFormatter(&logrus.TextFormatter{
+		ForceColors:   true,
+		FullTimestamp: true,
+	})
+
+	// Configures log output to os.Stdout (standard output).
+	// Output to stdout instead of the default stderr
+	logrus.SetOutput(os.Stdout)
+
+	// Sets the log level to InfoLevel - log informational warning msgs and above.
+	logrus.SetLevel(logrus.InfoLevel)
+}
 
 func main() {
 	ui := src.NewUI()
